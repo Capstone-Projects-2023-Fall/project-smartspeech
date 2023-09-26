a6006a9aae14b8500def50a215bda6b6ae212d2f
ruuffian <ruuffia@gmail.com>
2023-09-26 18:33:16 -0400

    Added a basic user/[id] route which returns the id given as a query parameter as a proof-of-concept. Time to Dockerize.

---
+++ frontend/src/pages/api/user/[id].ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type responseBody = {
+    user_id: string | string[]
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<responseBody>) {
+  const { id } = req.query;
+  if(id != undefined){
+    const response = { user_id: id };
+    res.status(200).json(response);
+  }
+}
