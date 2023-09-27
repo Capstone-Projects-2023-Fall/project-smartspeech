d286cbc929f8682ae03c89eecbcc7f6c7ec114cd
ruuffian <ruuffia@gmail.com>
2023-09-27 18:12:31 -0400

    Templated image endpoint for sending image data to an api and responding with the predicted word

--- frontend/src/pages/api/draw/[image].ts
+++ frontend/src/pages/api/draw/[image].ts
@@ -1,22 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
+type drawResponse = {
+  prediction: string;
+};
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<drawResponse>
 ) {
   const { image } = req.query;
   if (image) {
-    // Notify of success
-    res.status(200);
-    const url: string = "";
-    const prediction = await fetch(url, {
+    // Send image to XXX, recieve word describing picture
+    const imageUrl: string = "";
+    const imageResponse = await fetch(imageUrl, {
       method: "POST",
       headers: {
         "content-type": "application/json;charset=UTF-8",
       },
       body: JSON.stringify(image),
     });
-    
+    const prediction: drawResponse = { prediction: await imageResponse.json() };
+
+    res.status(200).send(prediction);
   } else {
     res.status(400);
   }
