fa5d90cedd412a34d1ad97760dbb81ba74707402
Parth099 <parthnpatel715@gmail.com>
2023-10-02 19:47:35 -0400

    defined the VPC and SG

---
+++ terraform/terraform.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "perma-terraform-state-bucket"
+    dynamodb_table = "state-lock-smartspeech"
+    key            = "SmartSpeech/TF/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    profile        = "personal-general"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "default_labels" {
+  type = map(string)
+  default = {
+    environment = "dev"
+    app         = "smart-speech"
+    sprint      = 2
+  }
+}
+
+variable "vpc_info" {
+  type = map(any)
+  default = {
+    "vpc_name" = "core_vpc"
+    "vpc_cidr" = "10.0.0.0/16"
+  }
+}
+
+variable "vpc_subnet_info" {
+  type = map(list(string))
+  default = {
+    "azs"                   = ["us-east-1a", "us-east-1b"]
+    "private_subnet_blocks" = ["10.0.128.0/18", "10.0.192.0/18"]
+    "public_subnet_blocks"  = ["10.0.0.0/18", "10.0.64.0/18"]
+  }
+}
