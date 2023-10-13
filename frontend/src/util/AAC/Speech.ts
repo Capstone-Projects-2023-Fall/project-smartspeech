a989a6a5b4189c50b7322e3a2666760439ccb31b
LandenLloyd <70919337+LandenLloyd@users.noreply.github.com>
2023-10-13 18:34:58 +0000

    Stub tts request

--- frontend/src/util/AAC/Speech.ts
+++ frontend/src/util/AAC/Speech.ts
@@ -1,13 +1,27 @@
 /**
  * Uses the Web Speech API to synthesise the text contained in sound
  */
-export function speakViaWebSpeechAPI(sound: string) {
-    if (!("speechSynthesis" in window)) {
+export async function speakViaWebSpeechAPI(sound: string) {
+    if (!window.AudioContext) {
         return;
     }
 
-    let utterance = new SpeechSynthesisUtterance(sound);
-    utterance.rate = +(process.env.NEXT_PUBLIC_VOICE_SPEED as string) ?? 1;
-    speechSynthesis.cancel();
-    speechSynthesis.speak(utterance);
+    // var context = new AudioContext();
+
+    // function playByteArray(bytes: ReadableStream<Uint8Array>) {
+    //     var buffer = new Uint8Array( bytes.length );
+    //     buffer.set( new Uint8Array(bytes), 0 );
+    
+    //     context.decodeAudioData(buffer.buffer, play);
+    // }
+    
+    // function play(audioBuffer: AudioBuffer) {
+    //     var source = context.createBufferSource();
+    //     source.buffer = audioBuffer;
+    //     source.connect( context.destination );
+    //     source.start(0);
+    // }
+    
+    let response = await fetch("https://jubilant-memory-rwvj7q64qxgfxr44-8000.app.github.dev/tts?" + new URLSearchParams("phrase=" + sound));
+    console.log(response.body)
 }
