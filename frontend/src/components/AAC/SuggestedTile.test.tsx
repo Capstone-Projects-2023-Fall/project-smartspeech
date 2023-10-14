201298b7e19da9f10d409df46927479a7f691739
Zeshan <tui79498@temple.edu>
2023-10-14 12:08:31 -0400

    making test for suggestTiles

---
+++ frontend/src/components/AAC/SuggestedTile.test.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/react"
+import{render} from "@testing-library/react"
+import SuggestedTiles from "./SuggestedTile"
+
+describe("Renders placeholder text for suggested tiles", ()=>{
+    it("should render", ()=>{
+        const renderPage = render(SuggestedTiles)
+        
+    })
+})
