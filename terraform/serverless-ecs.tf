068109d7dd160c656bed23992181b6595dcdd58b
Parth099 <parthnpatel715@gmail.com>
2023-10-15 14:17:15 -0400

    uncommented aws ALB and aws ECS service code

--- terraform/serverless-ecs.tf
+++ terraform/serverless-ecs.tf
@@ -82,94 +82,94 @@ resource "aws_ecs_task_definition" "backend_task_def" {
 
 }
 
-# module "alb" {
-#   count   = var.is_full_deployment ? 1 : 0
-#   source  = "terraform-aws-modules/alb/aws"
-#   version = "~> 8.4.0"
+module "alb" {
+  count   = var.is_full_deployment ? 1 : 0
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.4.0"
 
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.allow_web_sg.id]
-#   subnets            = module.vpc.public_subnets # drop ALB in public subnet
-#   vpc_id             = module.vpc.vpc_id
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_web_sg.id]
+  subnets            = module.vpc.public_subnets # drop ALB in public subnet
+  vpc_id             = module.vpc.vpc_id
 
-#   name = local.alb_name
+  name = local.alb_name
 
-#   # allows for TLS off loading where our internal traffic within AWS is insecure yet external traffic is HTTPS
-#   https_listeners = [
-#     {
-#       port               = 443
-#       protocol           = "HTTPS"
-#       certificate_arn    = data.aws_acm_certificate.backend_https_cert.id
-#       target_group_index = 0
-#     }
-#   ]
+  # allows for TLS off loading where our internal traffic within AWS is insecure yet external traffic is HTTPS
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = data.aws_acm_certificate.backend_https_cert.id
+      target_group_index = 0
+    }
+  ]
 
-#   target_groups = [
-#     {
-#       backend_port     = var.ecs_backend_container_info.container_port
-#       backend_protocol = "HTTP"
-#       target_type      = "ip"
-#       health_check = {
-#         enabled             = true
-#         interval            = 60
-#         path                = "/health-check" # check for 2XX on this route
-#         port                = var.ecs_backend_container_info.container_port
-#         healthy_threshold   = 3
-#         unhealthy_threshold = 3
-#         timeout             = 6
-#         protocol            = "HTTP"
-#         matcher             = "200-299"
-#       }
-#     }
-#   ]
+  target_groups = [
+    {
+      backend_port     = var.ecs_backend_container_info.container_port
+      backend_protocol = "HTTP"
+      target_type      = "ip"
+      health_check = {
+        enabled             = true
+        interval            = 60
+        path                = "/health-check" # check for 2XX on this route
+        port                = var.ecs_backend_container_info.container_port
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-299"
+      }
+    }
+  ]
 
-#   tags = merge(var.default_labels, {
-#     Name = local.alb_name
-#   })
+  tags = merge(var.default_labels, {
+    Name = local.alb_name
+  })
 
-#   target_group_tags = merge(var.default_labels, {
-#     Name = local.tg_name
-#   })
-# }
+  target_group_tags = merge(var.default_labels, {
+    Name = local.tg_name
+  })
+}
 
-# resource "aws_ecs_service" "ss_backend_service" {
-#   count           = var.is_full_deployment ? 1 : 0
-#   cluster         = module.ecs.cluster_id
-#   desired_count   = 1
-#   launch_type     = "FARGATE"
-#   name            = var.backend_service_info.name
-#   task_definition = aws_ecs_task_definition.backend_task_def.arn
+resource "aws_ecs_service" "ss_backend_service" {
+  count           = var.is_full_deployment ? 1 : 0
+  cluster         = module.ecs.cluster_id
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  name            = var.backend_service_info.name
+  task_definition = aws_ecs_task_definition.backend_task_def.arn
 
-#   lifecycle {
-#     ignore_changes = [desired_count] # Allow external changes to happen without Terraform conflicts, particularly around auto-scaling.
-#   }
+  lifecycle {
+    ignore_changes = [desired_count] # Allow external changes to happen without Terraform conflicts, particularly around auto-scaling.
+  }
 
-#   load_balancer {
-#     container_name   = var.ecs_backend_container_info.container_name
-#     container_port   = var.ecs_backend_container_info.container_port
-#     target_group_arn = module.alb[0].target_group_arns[0]
-#   }
+  load_balancer {
+    container_name   = var.ecs_backend_container_info.container_name
+    container_port   = var.ecs_backend_container_info.container_port
+    target_group_arn = module.alb[0].target_group_arns[0]
+  }
 
-#   network_configuration {
-#     security_groups = [aws_security_group.allow_web_sg.id]
-#     subnets         = module.vpc.private_subnets # drop service in priv subnet for security
-#   }
-# }
+  network_configuration {
+    security_groups = [aws_security_group.allow_web_sg.id]
+    subnets         = module.vpc.private_subnets # drop service in priv subnet for security
+  }
+}
 
-# resource "aws_route53_record" "backend_mapping" {
-#   count   = var.is_full_deployment ? 1 : 0
-#   zone_id = data.aws_route53_zone.backend_zone.zone_id
-#   name    = var.r53_domain_info.cert_domain
-#   type    = "CNAME"
-#   ttl     = 300
-#   records = [module.alb[0].lb_dns_name]
-# }
+resource "aws_route53_record" "backend_mapping" {
+  count   = var.is_full_deployment ? 1 : 0
+  zone_id = data.aws_route53_zone.backend_zone.zone_id
+  name    = var.r53_domain_info.cert_domain
+  type    = "CNAME"
+  ttl     = 300
+  records = [module.alb[0].lb_dns_name]
+}
 
-# output "url_with_out_https" {
-#   value = var.is_full_deployment ? "http://${module.alb[0].lb_dns_name}" : "NULL"
-# }
+output "url_with_out_https" {
+  value = var.is_full_deployment ? "http://${module.alb[0].lb_dns_name}" : "NULL"
+}
 
-# output "url_with_https" {
-#   value = var.is_full_deployment ? "https://${var.r53_domain_info.cert_domain}" : "NULL"
-# }
+output "url_with_https" {
+  value = var.is_full_deployment ? "https://${var.r53_domain_info.cert_domain}" : "NULL"
+}
 
