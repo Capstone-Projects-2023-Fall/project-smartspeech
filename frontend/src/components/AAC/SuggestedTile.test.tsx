a43151a099c769e2dccc40ecee54800d42fe7fb9
Parth Patel <33704202+Parth099@users.noreply.github.com>
2023-10-16 19:08:04 +0000

    fix failing test

--- frontend/src/components/AAC/SuggestedTile.test.tsx
+++ frontend/src/components/AAC/SuggestedTile.test.tsx
@@ -1,21 +1,19 @@
 import "@testing-library/jest-dom";
 import { render, screen} from "@testing-library/react";
 import SuggestedTiles from "./SuggestedTile";
-import { getAACAssets } from "@/util/AAC/getAACAssets";
 import { computeTileContainerName } from "./Tile";
-import sampleData from "@/data/testing/AAC/Tiles";
+import { mockSuggestedTileData } from "@/data/AAC/Tiles";
 
 jest.mock("../../util/AAC/getAACAssets");
 describe("Renders placeholder text for suggested tiles", ()=>{
     it("should render", ()=>{
-        (getAACAssets as jest.Mock).mockReturnValue(sampleData);
         render(<SuggestedTiles />);
 
         const container = screen.getByTestId("tiles-container");
-        const colorTileContainer = screen.getByTestId(computeTileContainerName(sampleData.colors.text));
+        const stopTileContainer = screen.getByTestId(computeTileContainerName(mockSuggestedTileData.stop.text));
 
         expect(container).toBeInTheDocument();
-        expect(colorTileContainer).toBeInTheDocument();
+        expect(stopTileContainer).toBeInTheDocument();
         
     })
 })
