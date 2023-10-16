6a43f8d2b8d2ff3c76b0a352ce4d0dad1d82169b
Zeshan <tui79498@temple.edu>
2023-10-16 09:01:34 -0400

    changing to simply return dataset tiles for now

--- frontend/src/components/AAC/SuggestedTile.tsx
+++ frontend/src/components/AAC/SuggestedTile.tsx
@@ -1,67 +1,45 @@
-import { FC, useEffect, useReducer, useState } from "react";
-import { useDraw } from "../../react-helpers/hooks/useDraw";
-import useClientRender from "@/react-helpers/hooks/useClientRender";
+import React, { useEffect, useReducer, useState } from "react";
+import { TileAssets } from "./TileTypes";
+import { stackReducer } from "@/react-state-management/reducers/stackReducer";
 import { getAACAssets } from "../../util/AAC/getAACAssets";
 import Tiles from "./Tiles";
-import { TileAssets } from "./TileTypes";
 import Tile from "./Tile";
-import { stackReducer } from "@/react-state-management/reducers/stackReducer";
-import data from "@/data/AAC/Tiles";
 
+/**
+ *
+ * @returns Component which will fetch tiles and display them based on what is drawn
+ * currently using AACAssets dataset
+ * update later to add logic for what is drawn on Canvas
+ */
+export default function SuggestedTiles() {
+  const [data, setData] = useState<TileAssets>({});
+  const [dataLocation, dispatch] = useReducer(stackReducer<string>, []);
+  const [currentFrame, setCurrentFrame] = useState<TileAssets>({});
 
+  useEffect(() => {
+    const tileAssets = getAACAssets();
+    setData(tileAssets);
+  }, []);
 
-export const BACK_BTN_TEXT = "back";
+  useEffect(() => {
+    if (dataLocation.length === 0) return setCurrentFrame(data);
+  }, [data, dataLocation]);
 
-export default function SuggestedTiles(){
-    const [data, setData] = useState<TileAssets>({});
-    const [dataLocation, dispatch] = useReducer(stackReducer<string>, []);
-    const [currentFrame, setCurrentFrame] = useState<TileAssets>({});
+  return (
+    <><h1 className="tilesHeaderFont">Suggested Tiles</h1>
+      <div className="grid grid-cols-8" data-testid="tiles-container">
+        {Object.keys(currentFrame).map((key) => {
+          const tileData = currentFrame[key];
+          const { image, text, sound, tileColor, subTiles } = tileData;
 
-    useEffect(() => {
-        const tileAssets = getAACAssets();
-        setData(tileAssets);
-    }, []);
-
-    useEffect(() => {
-        if (Object.keys(data).length === 0) return;
-        if (dataLocation.length === 0) return setCurrentFrame(data);
-
-        let newFrame = data;
-
-        dataLocation.forEach((loc) => {
-            if (loc in newFrame && newFrame[loc].subTiles) {
-                newFrame = newFrame[loc].subTiles as TileAssets;
-            }
-        });
-
-        setCurrentFrame(newFrame);
-    }, [data, dataLocation]);
-
-    
-    
-    return (
-        <><h1 className="tilesHeaderFont">Suggested Tiles</h1><div className="grid grid-cols-8" data-testid="tiles-container">
-            {Object.keys(currentFrame).map((key) => {
-                const tileData = currentFrame[key];
-                const { image, text, sound, tileColor, subTiles } = tileData;
-                const renderedTile = <Tile image={image} text={text} sound={subTiles ? "" : sound} tileColor={tileColor} />;
-
-                const clickHandler = subTiles
-                    ? // attach navigator if subtiles exist
-                    () => dispatch({
-                        type: "add",
-                        payload: key,
-                    })
-                    : // go back to main menu if button clicked
-                    () => dispatch({ type: "clear" });
-
-                return (
-                    <div key={key} onClick={clickHandler}>
-                        {renderedTile}
-                    </div>
-                );
-            })}
-           
-        </div></>
-    );
+          //rendering tiles and setting up click handler to add key to data location
+          return (
+            <div key={key} onClick={() => dispatch({ type: "add", payload: key })}>
+              <Tile image={image} text={text} sound={sound} tileColor={tileColor} />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
