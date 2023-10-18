b71977c6d11454a7af29afc9e0bcb76408752c30
Landen Lloyd <oldestoflloyd@gmail.com>
2023-10-18 10:57:26 -0400

    Use Cache API to save audio

--- frontend/src/util/AAC/Speech.ts
+++ frontend/src/util/AAC/Speech.ts
@@ -3,44 +3,52 @@
  * by creating an AudioBufferSourceNode, as described here:
  * https://developer.mozilla.org/en-US/docs/Web/API/Response/arrayBuffer#playing_music
  *
+ * Previous calls are stored in the main thread Cache, as recommended here:
+ * https://web.dev/articles/storage-for-the-web
+ *
  * @returns true if the sound is played, and false otherwise
  */
-export function speakViaWebSpeechAPI(sound: string): boolean {
+export async function speakViaWebSpeechAPI(sound: string): Promise<boolean> {
   if (!window.AudioContext) {
     return false;
   }
 
-  let context = new AudioContext();
+  // Get Response by first checking cache
   let backendURL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
+  let request = new Request(
+    backendURL + "/tts?" + new URLSearchParams("phrase=" + sound)
+  );
+  let cache = await caches.open("tts-cache");
+
+  let response = await cache.match(request);
+  if (response === undefined) {
+    response = await fetch(request);
+    if (response.ok) {
+      cache.put(request, response.clone());
+    } else {
+      return false;
+    }
+  }
+
+  // Play audio in response out loud
+  let context = new AudioContext();
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Response/arrayBuffer#playing_music
-  // Get mp3 bytes from backend
-  try {
-    fetch(backendURL + "/tts?" + new URLSearchParams("phrase=" + sound))
-      // Retrieve bytes from response
-      .then((response) => {
-        if (response.ok) {
-          return response.arrayBuffer();
-        } else {
-          throw new Error("Could not retrieve audio from backend.");
-        }
-      })
-      // Convert bytes to audio data
-      .then((buffer) => context.decodeAudioData(buffer))
-      // Create an audio source
-      .then((decodedData) => {
-        const source = new AudioBufferSourceNode(context);
-        source.buffer = decodedData;
-        source.connect(context.destination);
-        return source;
-      })
-      // Play audio source
-      .then((source) => {
-        source.start(0);
-      });
+  response
+    .arrayBuffer()
+    // Convert bytes to audio data
+    .then((buffer) => context.decodeAudioData(buffer))
+    // Create an audio source
+    .then((decodedData) => {
+      const source = new AudioBufferSourceNode(context);
+      source.buffer = decodedData;
+      source.connect(context.destination);
+      return source;
+    })
+    // Play audio source
+    .then((source) => {
+      source.start(0);
+    });
 
-    return true;
-  } catch {
-    return false;
-  }
+  return true;
 }
