f7a87856fbe6f8fc939bc4cb5d9b33487df29e54
Landen Lloyd <oldestoflloyd@gmail.com>
2023-10-18 12:16:21 -0400

    Factor out request logic and improve docs

--- frontend/src/util/AAC/Speech.ts
+++ frontend/src/util/AAC/Speech.ts
@@ -1,22 +1,11 @@
 /**
- * Retrieves a voice mp3 saying `sound` from the backend, then plays the audio
- * by creating an AudioBufferSourceNode, as described here:
- * https://developer.mozilla.org/en-US/docs/Web/API/Response/arrayBuffer#playing_music
- *
- * Previous calls are stored in the main thread Cache, as recommended here:
- * https://web.dev/articles/storage-for-the-web
- *
- * @returns true if the sound is played, and false otherwise
+ * Returns a Response containing an mp3 speaking `phrase`. Checks the browser
+ * cache to see if a Response is already saved.
  */
-export async function speakViaWebSpeechAPI(sound: string): Promise<boolean> {
-  if (!window.AudioContext) {
-    return false;
-  }
-
-  // Get Response by first checking cache
+async function requestTTS(phrase: string): Promise<Response | undefined> {
   let backendURL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
   let request = new Request(
-    backendURL + "/tts?" + new URLSearchParams("phrase=" + sound)
+    backendURL + "/tts?" + new URLSearchParams("phrase=" + phrase)
   );
   let cache = await caches.open("tts-cache");
 
@@ -26,8 +15,37 @@ export async function speakViaWebSpeechAPI(sound: string): Promise<boolean> {
     if (response.ok) {
       cache.put(request, response.clone());
     } else {
-      return false;
+      return undefined;
     }
+  }
+
+  return response;
+}
+
+/**
+ * Retrieves a voice mp3 saying `sound` from the backend, then plays the audio
+ * by creating an AudioBufferSourceNode, as described here:
+ * https://developer.mozilla.org/en-US/docs/Web/API/Response/arrayBuffer#playing_music
+ *
+ * Previous calls are stored in the main thread Cache, as recommended here:
+ * https://web.dev/articles/storage-for-the-web
+ *
+ * "Why do we cache the Response and not the AudioBuffer?"
+ * The AudioBuffer is sampled according to the AudioContext, so it
+ * is most reliable is we resample the AudioBuffer each call. This
+ * extra processing time is not noticeable by the end user.
+ *
+ * @returns true if the sound is played, and false otherwise
+ */
+export async function speakViaWebSpeechAPI(sound: string): Promise<boolean> {
+  if (!window.AudioContext) {
+    return false;
+  }
+
+  // Get Response by first checking cache
+  let response = await requestTTS(sound);
+  if (response === undefined) {
+    return false;
   }
 
   // Play audio in response out loud
