6988d5b84244e934310fd0f26e16eacb69419ecc
Parth099 <parthnpatel715@gmail.com>
2023-10-21 20:28:54 -0400

    moved constants out to one file

--- backend/src/routers/s3.py
+++ backend/src/routers/s3.py
@@ -8,19 +8,21 @@ import boto3
 from base64 import b64decode
 from io import BytesIO
 
-BUCKET_NAME_ENV_VAR = "BUCKET_NAME"
-ACCESS_KEY_ENV_VAR = "ACCESS_KEY"
-SECRET_KEY_ENV_VAR = "SECRET_KEY"
-
-UPLOAD_TO_S3_ROUTE = "/s3/drawn-image"
+from .s3_constants import BUCKET_NAME_ENV_VAR, ACCESS_KEY_ENV_VAR, SECRET_KEY_ENV_VAR, UPLOAD_TO_S3_ROUTE
 
 router = APIRouter()
 load_dotenv(dotenv_path=".env.local")
 
+if getenv(ACCESS_KEY_ENV_VAR) is None or getenv(SECRET_KEY_ENV_VAR) is None:
+	print(getenv(ACCESS_KEY_ENV_VAR), getenv(SECRET_KEY_ENV_VAR), uuid.uuid4().hex)
+	raise RuntimeError("AWS Creds or ENV not present")
+
 s3 = boto3.client('s3', \
-					aws_access_key_id=getenv(ACCESS_KEY_ENV_VAR), \
-					aws_secret_access_key=getenv(SECRET_KEY_ENV_VAR)
+					aws_access_key_id=getenv(ACCESS_KEY_ENV_VAR, "key"), \
+					aws_secret_access_key=getenv(SECRET_KEY_ENV_VAR, "secret")
 				)
+
+
 
 class UploadFileToS3Model(BaseModel):
 	base64File: str
@@ -32,7 +34,8 @@ def upload_file_to_s3(body: UploadFileToS3Model):
 	uploaded_file_name = f'{uuid.uuid4().hex}-{body.file_name}.{body.extension}'
 	file_binary = b64decode(body.base64File)
 	
-	s3.upload_fileobj(BytesIO(file_binary), getenv(BUCKET_NAME_ENV_VAR), uploaded_file_name)
+	
+	s3.upload_fileobj(BytesIO(file_binary), getenv(BUCKET_NAME_ENV_VAR, "smart-speech-media"), uploaded_file_name)
 
 	return {
 		"message": "uploaded",
