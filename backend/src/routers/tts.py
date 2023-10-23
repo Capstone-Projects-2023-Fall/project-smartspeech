55c400a0e056bc1583db70a2f5935ecfb47ca929
Landen Lloyd <oldestoflloyd@gmail.com>
2023-10-23 10:57:53 -0400

    Clean up tts route definition

--- backend/src/routers/tts.py
+++ backend/src/routers/tts.py
@@ -25,19 +25,16 @@ def get_session():
     return requests.Session()
 
 
-@router.get(TTS_ROUTE)
-async def tts(phrase: str, config: Annotated[dict, Depends(get_config)], session: Annotated[requests.Session, Depends(get_session)]):
-    """
-    Converts **phrase** to audio using elevenlab's test to speech service.
-    Returns a string of mp3 bytes.
-    """
-    headers = {
+def get_headers(config: dict) -> dict:
+    return {
         "Accept": "audio/mpeg",
         "Content-Type": "application/json",
         "xi-api-key": config["TTS_API_KEY"]
     }
 
-    data = {
+
+def get_data(phrase: str) -> dict:
+    return {
         "text": phrase,
         "model_id": "eleven_monolingual_v1",
         "voice_settings": {
@@ -46,11 +43,17 @@ async def tts(phrase: str, config: Annotated[dict, Depends(get_config)], session
         }
     }
 
-    print(headers)
+
+@router.get(TTS_ROUTE)
+async def tts(phrase: str, config: Annotated[dict, Depends(get_config)], session: Annotated[requests.Session, Depends(get_session)]):
+    """
+    Converts **phrase** to audio using elevenlab's test to speech service.
+    Returns a string of mp3 bytes.
+    """
 
     start = time.perf_counter()
     response = session.post(
-        config["TTS_API_URL"], json=data, headers=headers)
+        config["TTS_API_URL"], json=get_data(phrase), headers=get_headers(config))
     end = time.perf_counter()
 
     logging.debug(f"Converting phrase '{phrase}' to audio took {end-start}s.")
