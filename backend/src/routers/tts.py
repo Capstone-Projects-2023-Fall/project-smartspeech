9fbb668019d827a0964e4ab499fd17334b7d76fe
Landen Lloyd <oldestoflloyd@gmail.com>
2023-10-24 20:10:36 -0400

    Cache speech files in S3 on the backend

--- backend/src/routers/tts.py
+++ backend/src/routers/tts.py
@@ -1,12 +1,15 @@
+import base64
 from functools import cache
-import logging
 import time
 from typing import Annotated
+import urllib.parse
 
 from dotenv import dotenv_values
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends, Response, HTTPException
 import requests
 
+from .s3_constants import GET_FROM_S3_ROUTE
+from .s3 import get_file_from_s3_logic, upload_file_to_s3_logic
 
 TTS_ROUTE = "/tts"
 
@@ -44,21 +47,49 @@ def get_data(phrase: str) -> dict:
     }
 
 
+def phrase_to_s3_name(phrase: str) -> str:
+    """Lowercases `phrase`, removes all whitespace, and adds the mp3 extension"""
+    return ''.join(phrase.lower().split()) + '.mp3'
+
+
 @router.get(TTS_ROUTE)
 async def tts(phrase: str, config: Annotated[dict, Depends(get_config)], session: Annotated[requests.Session, Depends(get_session)]):
     """
-    Converts **phrase** to audio using elevenlab's test to speech service.
+    Checks if the audio of **phrase** is cached on S3, and returns. If it is
+    not on S3, then it converts **phrase** to audio using elevenlab's test to
+    speech service.
+
     Returns a string of mp3 bytes.
     """
-
     start = time.perf_counter()
-    response = session.post(
-        config["TTS_API_URL"], json=get_data(phrase), headers=get_headers(config))
+
+    audio = None
+
+    try:
+        # Try to fetch from cache
+        print('Trying to get phrase from S3...')
+        (object_content, _) = get_file_from_s3_logic(
+            phrase_to_s3_name(phrase), get_url=False)
+        print('Object found on S3!')
+        audio = b"".join(object_content)
+    except HTTPException:
+        # Cache miss, so we generate a new sound file
+        print('Object not found on S3! Fetching from elevenlabs...')
+        response = session.post(
+            config["TTS_API_URL"], json=get_data(phrase), headers=get_headers(config))
+        response.raise_for_status()
+        audio = b"".join(response.iter_content(chunk_size=1024))
+
+        # Add the new sound file to S3
+        try:
+            upload_file_to_s3_logic(audio, phrase_to_s3_name(phrase), False)
+        except Exception as e:
+            # Even if the upload was unsuccessful,
+            # we still return the audio to the user
+            print(f'Failed to upload phrase audio. {e}')
+
     end = time.perf_counter()
 
-    logging.debug(f"Converting phrase '{phrase}' to audio took {end-start}s.")
+    print(f"Converting phrase '{phrase}' to audio took {end-start}s.")
 
-    response.raise_for_status()
-
-    audio = b"".join(response.iter_content(chunk_size=1024))
     return Response(audio)
