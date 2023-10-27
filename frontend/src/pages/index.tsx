43813365a67cb966b7ee9abacd7d7f8df97a220d
Parth099 <parthnpatel715@gmail.com>
2023-10-27 15:56:39 -0400

    fixed up ss83 and added tests

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,12 @@ import UtteredTilesProvider from "@/react-state-management/providers/useUtteredT
 import Canvas from "@/components/AAC/Canvas";
 import SuggestedTiles from "@/components/AAC/SuggestedTile";
 import RecentlyClickedTiles from "@/components/AAC/RecentlyClickedTiles";
+import Tile from "@/components/AAC/Tile";
+
+export const ManualModeTestIds = {
+    manualBtn: "manual-button",
+    exitManualBtn: "return-button",
+};
 
 /**
  *
@@ -21,28 +27,32 @@ export default function Home() {
         setShowTiles(false);
     };
 
+    const itemsShownByDefault = (
+        <>
+            <Canvas />
+            <RecentlyClickedTiles />
+            <SuggestedTiles />
+        </>
+    );
+
     return (
         <section className="font-inter">
             <UtteredTilesProvider>
                 <SelectedTilesActionBar />
                 {!showTiles && (
                     <>
-                        <Canvas />
-                        <RecentlyClickedTiles />
-                        <SuggestedTiles />
-                        <Tiles />
-                        <button className="tile-button" onClick={handleTilesClick} data-testid="manual-button">
-                            <img src={"/AAC_assets/img/standard/manual.png"} width={176} height={176} />
-                        </button>
+                        {itemsShownByDefault}
+                        <div onClick={handleTilesClick} data-testid={ManualModeTestIds.manualBtn}>
+                            <Tile image="/AAC_assets/img/standard/manual.png" text="" tileColor="blue" />
+                        </div>
                     </>
                 )}
                 {showTiles && (
                     <>
                         <Tiles />
-                        <button className="tile-button" onClick={handleTilesReturn} data-testid="return-button">
-                            <h1 className="tilesHeaderFont">Return to Canvas</h1>
-                            <img src={"/AAC_assets/img/standard/back_arrow.png"} width={176} height={176} />
-                        </button>
+                        <div onClick={handleTilesReturn} data-testid={ManualModeTestIds.exitManualBtn}>
+                            <Tile image="/AAC_assets/img/standard/back_arrow.png" text="Return" tileColor="blue" />
+                        </div>
                     </>
                 )}
             </UtteredTilesProvider>
