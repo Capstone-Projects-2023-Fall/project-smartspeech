33486473d9ee8b10366437517cac5b81f5527030
Landen Lloyd <oldestoflloyd@gmail.com>
2023-11-03 10:58:42 -0400

    Add tiles under the category "They" to the manual board

---
+++ frontend/src/data/AAC/who/who.ts
@@ -0,0 +1,74 @@
+import { TileAssets } from "@/components/AAC/TileTypes";
+
+export const WHO_TILES_COLOR = "purple";
+
+const who: TileAssets = {
+  who: {
+    image: "/AAC_assets/img/who/who.png",
+    text: "Who",
+    sound: "Who",
+    tileColor: WHO_TILES_COLOR,
+  },
+  him: {
+    image: "/AAC_assets/img/they/boy.png",
+    text: "Him",
+    sound: "Him",
+    tileColor: WHO_TILES_COLOR,
+  },
+  his: {
+    image: "/AAC_assets/img/they/he.png",
+    text: "His",
+    sound: "His",
+    tileColor: WHO_TILES_COLOR,
+  },
+  her: {
+    image: "/AAC_assets/img/they/girl.png",
+    text: "Her",
+    sound: "Her",
+    tileColor: WHO_TILES_COLOR,
+  },
+  hers: {
+    image: "/AAC_assets/img/they/she.png",
+    text: "Hers",
+    sound: "Hers",
+    tileColor: WHO_TILES_COLOR,
+  },
+  us: {
+    image: "/AAC_assets/img/who/us.png",
+    text: "Us",
+    sound: "Us",
+    tileColor: WHO_TILES_COLOR,
+  },
+  theirs: {
+    image: "/AAC_assets/img/who/theirs.png",
+    text: "Theirs",
+    sound: "Theirs",
+    tileColor: WHO_TILES_COLOR,
+  },
+  myself: {
+    image: "/AAC_assets/img/who/myself.png",
+    text: "Myself",
+    sound: "Myself",
+    tileColor: WHO_TILES_COLOR,
+  },
+  people: {
+    image: "/AAC_assets/img/who/people.png",
+    text: "People",
+    sound: "People",
+    tileColor: WHO_TILES_COLOR,
+  },
+  man: {
+    image: "/AAC_assets/img/who/man.png",
+    text: "Man",
+    sound: "Man",
+    tileColor: WHO_TILES_COLOR,
+  },
+  woman: {
+    image: "/AAC_assets/img/who/woman.png",
+    text: "Woman",
+    sound: "Woman",
+    tileColor: WHO_TILES_COLOR,
+  },
+};
+
+export default who;
