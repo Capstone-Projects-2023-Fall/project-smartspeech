0d6f228cb34dfcfa49afe72da1b5124d4005e31b
Zeshan <tui79498@temple.edu>
2023-11-07 22:39:12 -0500

    setting up suggestedTile with backend request

--- frontend/src/components/AAC/SuggestedTile.tsx
+++ frontend/src/components/AAC/SuggestedTile.tsx
@@ -15,7 +15,30 @@ import { useRekognition } from "@/react-state-management/providers/useRekognitio
 export default function SuggestedTiles() {
     // State to store tile data and current location
     const { items } = useRekognition();
+    const [suggestions, setSuggestions] = useState([]);
 
+    const fetchSuggestions = async() => {
+        try{
+            const responce = await fetch('http://localhost:3000/similarity',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                //might need to make it bigger
+                body: JSON.stringify({words: ['fruit', 'apple', 'orange']})
+            });
+            if(responce.ok){
+                //parsing through backend JSON
+                const data = await responce.json();
+                setSuggestions(data.suggestions);
+            } else {
+                console.error('Request fail');
+            }
+        } catch (error){
+            console.error('Error:', error);
+        }
+    }
+    
     return (
         <>
             <h1 className="tilesHeaderFont">Suggested Tiles</h1>
@@ -24,6 +47,10 @@ export default function SuggestedTiles() {
                     const { image, text, sound, tileColor } = item;
                     return <Tile image={image} text={text} sound={sound} tileColor={tileColor} key={`${item.text}-${i}`} />;
                 })}
+            </div>
+            
+            <div className="flex flex-cols-8 gap-6" data-testid="drawling-suggestion">
+                
             </div>
         </>
     );
