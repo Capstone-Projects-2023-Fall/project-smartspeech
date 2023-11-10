622a93806bac6f2c3cf75aba82b77111413fd669
Zeshan <tui79498@temple.edu>
2023-11-10 18:36:07 -0500

    moving progress to SuggestedTiles Component

--- frontend/src/components/AAC/SuggestedTile.tsx
+++ frontend/src/components/AAC/SuggestedTile.tsx
@@ -1,25 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import Tile from "./Tile";
 import { useSuggestedTilesContext } from "@/react-state-management/providers/SuggestedTilesProvider";
 
 /**
- *
- * @returns Component which will fetch tiles and display them based on what is drawn
- * currently using AACAssets dataset
- * update later to add logic for what is drawn on Canvas
+ * @returns Component that fetches suggested tiles based on user input and displays them
  */
 export default function SuggestedTiles() {
-    const { tiles } = useSuggestedTilesContext();
+  const { tiles } = useSuggestedTilesContext();
+  const [inputWords, setInputWords] = useState<string>('');
+  const [suggestedTiles, setSuggestedTiles] = useState<string[]>([]);
 
-    return (
-        <section className="">
-            <h1 className="tilesHeaderFont">Suggested Tiles</h1>
-            <div className="flex flex-cols-8 gap-6" data-testid="tiles-container">
-                {tiles.map((item, i) => {
-                    const { image, text, sound, tileColor } = item;
-                    return <Tile image={image} text={text} sound={sound} tileColor={tileColor} key={`${item.text}-${i}`} />;
-                })}
-            </div>
-        </section>
-    );
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputWords(event.target.value);
+  };
+
+  const handleButtonClick = async () => {
+    try {
+      // Split the input string into an array of words
+      const wordsArray = inputWords.split(/\s+/).filter(Boolean);
+
+      // Create a data object with the words
+      const data = { words: wordsArray };
+
+      // Make a POST request to the similarity backend
+      const response = await fetch('http://localhost:8000/similarity', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      // Parse the JSON response
+      const result = await response.json();
+
+      // Get the five most suggested tiles
+      const topFiveSuggestions = result.suggestions.slice(0, 5);
+
+      // Update the state with the suggested tiles
+      setSuggestedTiles(topFiveSuggestions);
+    } catch (error) {
+      // Handle errors during the API request
+      console.error('Error:', error);
+      setSuggestedTiles(['Error fetching suggestions.']);
+    }
+  };
+
+  return (
+    <section className="">
+      <h1 className="tilesHeaderFont">Suggested Tiles</h1>
+      <div className="flex flex-cols-8 gap-6" data-testid="tiles-container">
+        {/* Input for entering words */}
+        <input type="text" value={inputWords} onChange={handleInputChange} placeholder="Enter words..." />
+
+        {/* Button to trigger the API request */}
+        <button type="button" onClick={handleButtonClick}>
+          Get Similar Words
+        </button>
+
+        {/* Display the suggested tiles */}
+        {suggestedTiles.length > 0 ? (
+          <ul>
+            {/* Display each suggested tile as a list item */}
+            {suggestedTiles.map((suggestion, index) => (
+              <li key={index}>{suggestion}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No suggestions found.</p>
+        )}
+      </div>
+
+      {/* Display the suggested tiles based on the context */}
+      {tiles.map((item, i) => {
+        const { image, text, sound, tileColor } = item;
+        return <Tile image={image} text={text} sound={sound} tileColor={tileColor} key={`${item.text}-${i}`} />;
+      })}
+    </section>
+  );
 }
