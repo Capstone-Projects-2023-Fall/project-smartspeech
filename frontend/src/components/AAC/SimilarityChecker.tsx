2f045c4f1cfecbcf6f119177dc566a1ab008e20e
Zeshan <tui79498@temple.edu>
2023-11-10 12:17:58 -0500

    making a simpler variation for testing

--- frontend/src/components/AAC/SimilarityChecker.tsx
+++ frontend/src/components/AAC/SimilarityChecker.tsx
@@ -1,75 +1,62 @@
 import React, { useState } from 'react';
-
 interface SimilarityResponse {
-  suggestions: string[];
-}
-
-interface SimilarityFormData {
-  words: string;
-}
-
-const SimilarityChecker: React.FC = () => {
-  const [inputWords, setInputWords] = useState<string>('');
-  const [suggestions, setSuggestions] = useState<string[]>([]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputWords(event.target.value);
-  };
-
-  const getSimilarWords = async () => {
-    try {
-      // Convert the input string to a list of words
-      const wordsList = inputWords.split(',').map((word) => word.trim());
-
-      // Create a data object with the words
-      const data: SimilarityFormData = { words: wordsList.join(',') };
-
-      // Make a POST request to the API
-      const response = await fetch('http://localhost:8000/similarity', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      //Parse the JSON response
-      const result: SimilarityResponse = await response.json();
-      setSuggestions(result.suggestions);
-    } catch (error) {
-      console.error('Error:', error);
-      setSuggestions(['Error fetching suggestions.']);
-    }
-  };
-
-  return (
-    <div>
-      <h1>Word Similarity Checker</h1>
-      <label htmlFor="wordInput">Enter words (comma-separated):</label>
-      <input
-        type="text"
-        id="wordInput"
-        value={inputWords}
-        onChange={handleInputChange}
-        required
-      />
-      <button type="button" onClick={getSimilarWords}>
-        Get Similar Words
-      </button>
-
+    suggestions: string[];
+  }
+  
+  const SimilarityChecker: React.FC = () => {
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+  
+    const getSimilarWords = async () => {
+      try {
+        // Set of words to send to the API
+        const wordsToCheck = ['apple', 'banana', 'orange'];
+  
+        // Create a data object with the words
+        const data = { words: wordsToCheck };
+  
+        // Make a POST request to the API
+        const response = await fetch('http://localhost:8000/similarity', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+  
+        // Parse the JSON response
+        const result: SimilarityResponse = await response.json();
+        setSuggestions(result.suggestions);
+      } catch (error) {
+        // Handle errors during the API request
+        console.error('Error:', error);
+        setSuggestions(['Error fetching suggestions.']);
+      }
+    };
+  
+    return (
       <div>
-        {suggestions.length > 0 ? (
-          <ul>
-            {suggestions.map((suggestion, index) => (
-              <li key={index}>{suggestion}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No suggestions found.</p>
-        )}
+        <h1>Word Similarity Checker</h1>
+  
+        {/* Button to trigger the API request */}
+        <button type="button" onClick={getSimilarWords}>
+          Get Similar Words
+        </button>
+  
+        {/* Display the suggestions */}
+        <div>
+          {suggestions.length > 0 ? (
+            <ul>
+              {/* Display each suggestion as a list item */}
+              {suggestions.map((suggestion, index) => (
+                <li key={index}>{suggestion}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No suggestions found.</p>
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default SimilarityChecker;
+    );
+  };
+  
+  export default SimilarityChecker;
