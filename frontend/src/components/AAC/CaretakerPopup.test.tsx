ff9f43070eef8823b4b9e5a43b9aa7e2156e14ef
ruuffian <ruuffia@gmail.com>
2023-11-27 17:41:17 -0500

    Still failing all tests

--- frontend/src/components/AAC/CaretakerPopup.test.tsx
+++ frontend/src/components/AAC/CaretakerPopup.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import CaretakerPopup, { CaretakerPopupTestIds } from "./CaretakerPopup";
 import CaretakerPopupProvider from "@/react-state-management/providers/CaretakerPopupProvider";
 import { CaretakerPopupTitleTestIds } from "./CaretakerPopupTitle";
@@ -8,34 +8,29 @@ import { CaretakerPopupBodyTestIds } from "./CaretakerPopupBody";
 
 export const tests = describe("Caretaker Popup: [CaretakerPopup]", () => {
     it("should render", () => {
-        // const setState = jest.fn(() => true);
-        // const renderPopup = true;
-        // jest
-        // .spyOn(React, 'useState')
-        // .mockImplementation(() => [renderPopup, setState]);
+        jest.useFakeTimers();
+        jest.spyOn(global, 'setTimeout');
+
         render(
             <CaretakerPopupProvider>
                 <CaretakerPopup />
             </CaretakerPopupProvider>
         )
 
-        const timeoutId = setTimeout(() => {
-            const caretakerPopupContainer = screen.getByTestId(CaretakerPopupTestIds.mainWindow);
-            const titleComponent = screen.getByTestId(CaretakerPopupTitleTestIds.titleContainer);
-            const bodyComponent = screen.getByTestId(CaretakerPopupBodyTestIds.bodyContainer);
-            const closeCaretakerPopupBtn = screen.getByTestId(CaretakerPopupTitleTestIds.closePopupBtn);
-            const okCaretakerPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.okPopupBtn);
-            const doNotShowPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.doNotShowBtn);
+        const caretakerPopupContainer = screen.getByTestId(CaretakerPopupTestIds.mainWindow);
+        const titleComponent = screen.getByTestId(CaretakerPopupTitleTestIds.titleContainer);
+        const bodyComponent = screen.getByTestId(CaretakerPopupBodyTestIds.bodyContainer);
+        const closeCaretakerPopupBtn = screen.getByTestId(CaretakerPopupTitleTestIds.closePopupBtn);
+        const okCaretakerPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.okPopupBtn);
+        const doNotShowPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.doNotShowBtn);
 
-            expect(caretakerPopupContainer).toBeInTheDocument();
-            expect(titleComponent).toBeInTheDocument();
-            expect(bodyComponent).toBeInTheDocument();
-            expect(closeCaretakerPopupBtn).toBeInTheDocument();
-            expect(okCaretakerPopupBtn).toBeInTheDocument();
-            expect(doNotShowPopupBtn).toBeInTheDocument();
-        }, 2000);
+        expect(caretakerPopupContainer).toBeInTheDocument();
+        expect(titleComponent).toBeInTheDocument();
+        expect(bodyComponent).toBeInTheDocument();
+        expect(closeCaretakerPopupBtn).toBeInTheDocument();
+        expect(okCaretakerPopupBtn).toBeInTheDocument();
+        expect(doNotShowPopupBtn).toBeInTheDocument();
 
-        clearTimeout(timeoutId);
     
     });
 
@@ -46,14 +41,12 @@ export const tests = describe("Caretaker Popup: [CaretakerPopup]", () => {
             </CaretakerPopupProvider>
         );
         
-        const timeoutId = setTimeout(() => {
-            const closeCaretakerPopupBtn = screen.getByTestId(CaretakerPopupTitleTestIds.closePopupBtn);
-            // popup will be closed
-            fireEvent.click(closeCaretakerPopupBtn)
-            expect(screen.queryByTestId(CaretakerPopupTestIds.mainWindow)).not.toBeInTheDocument()
-        }, 2000);
-        
-        clearTimeout(timeoutId);
+        const closeCaretakerPopupBtn = screen.getByTestId(CaretakerPopupTitleTestIds.closePopupBtn);
+        // popup will be closed
+        act(() => {
+            fireEvent.click(closeCaretakerPopupBtn);
+            expect(screen.queryByTestId(CaretakerPopupTestIds.mainWindow)).not.toBeInTheDocument();
+        });
     });
         
     
@@ -64,20 +57,17 @@ export const tests = describe("Caretaker Popup: [CaretakerPopup]", () => {
             </CaretakerPopupProvider>
         );
 
-        const timeoutId = setTimeout(() => {
-            const caretakerPopupContainer = screen.getByTestId(CaretakerPopupTestIds.mainWindow);
-            expect(caretakerPopupContainer).toBeInTheDocument();
-    
-            const okCaretakerPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.okPopupBtn);
-            expect(okCaretakerPopupBtn).toBeInTheDocument();
-            
-            // popup will be closed
-            fireEvent.click(okCaretakerPopupBtn);
-            
-            expect(screen.queryByTestId(CaretakerPopupTestIds.mainWindow)).not.toBeInTheDocument();
-        }, 2000);
+        const caretakerPopupContainer = screen.getByTestId(CaretakerPopupTestIds.mainWindow);
+        expect(caretakerPopupContainer).toBeInTheDocument();
+
+        const okCaretakerPopupBtn = screen.getByTestId(CaretakerPopupBodyTestIds.okPopupBtn);
+        expect(okCaretakerPopupBtn).toBeInTheDocument();
         
-        clearTimeout(timeoutId);
+        // popup will be closed
+        act(() => {
+        fireEvent.click(okCaretakerPopupBtn)
+        expect(screen.queryByTestId(CaretakerPopupTestIds.mainWindow)).not.toBeInTheDocument();
+        });
     });
 
     it("should disable popups when doNotShow button is clicked", () => {
@@ -87,20 +77,17 @@ export const tests = describe("Caretaker Popup: [CaretakerPopup]", () => {
             </CaretakerPopupProvider>
         );
 
-        const timeoutId = setTimeout(() => {
             const doNotShowBtn = screen.getByTestId(CaretakerPopupBodyTestIds.doNotShowBtn);
             expect(doNotShowBtn).toBeInTheDocument();
             
             // popup will be closed
-            fireEvent.click(doNotShowBtn);
-            const userPreference = window.localStorage.getItem("SHOW_CARETAKER_POPUP");
-            expect(userPreference).not.toBeNull();
-            expect(userPreference).not.toBeNaN();
-            expect(userPreference).not.toBeDefined();
-            expect(userPreference).toBeFalsy();
-
-        }, 2000);
-        
-        clearTimeout(timeoutId);
+            act(() => {
+                fireEvent.click(doNotShowBtn);
+                const userPreference = window.localStorage.getItem("SHOW_CARETAKER_POPUP");
+                expect(userPreference).not.toBeNull();
+                expect(userPreference).not.toBeNaN();
+                expect(userPreference).not.toBeDefined();
+                expect(userPreference).toBeFalsy();
+            });
     });
 });
