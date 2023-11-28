93516e99f8184ed57482f0e001543095a751839e
Parth099 <parthnpatel715@gmail.com>
2023-11-27 22:15:52 -0500

    regroup tests

--- backend/src/routers/tests/test_customtiles.py
+++ backend/src/routers/tests/test_customtiles.py
@@ -77,13 +77,6 @@ def client_valid_SQL_Connection_sql_query_error():
 
 	return TestClient(app)
 
-def test_upload_custom_tile_failure(client_invalid_SQL_Connection):
-	URL_WITH_EMAIL_QUERY = f'{aws_constants.GET_CUSTOM_TILES}?email={VALID_EMAIL}'
-	resp = client_invalid_SQL_Connection.get(URL_WITH_EMAIL_QUERY)
-
-	assert resp.status_code == 500
-	assert resp.json()['detail'] == sql_constants.DB_CONNECT_FAILURE_MSG
-
 def test_upload_custom_tile_success(client_valid_SQL_Connection):
 	URL_WITH_EMAIL_QUERY = f'{aws_constants.GET_CUSTOM_TILES}?email={VALID_EMAIL}'
 	resp = client_valid_SQL_Connection.get(URL_WITH_EMAIL_QUERY)
@@ -100,8 +93,6 @@ def test_upload_custom_tile_invalid_email(client_valid_SQL_Connection):
 	resp = client_valid_SQL_Connection.get(URL_WITH_EMAIL_QUERY)
 
 	assert resp.status_code == 400
-
-	# responses are in list format
 	assert resp.json()['detail'] == sql_constants.EMAIL_INVALID_MSG
 
 def test_upload_custom_tile_internal_query_error(client_valid_SQL_Connection_sql_query_error):
@@ -110,3 +101,10 @@ def test_upload_custom_tile_internal_query_error(client_valid_SQL_Connection_sql
 
 	assert resp.status_code == 500
 	assert resp.json()['detail'] == sql_constants.DB_GET_TILES_FAILURE_MSG
+
+def test_upload_custom_tile_failure(client_invalid_SQL_Connection):
+	URL_WITH_EMAIL_QUERY = f'{aws_constants.GET_CUSTOM_TILES}?email={VALID_EMAIL}'
+	resp = client_invalid_SQL_Connection.get(URL_WITH_EMAIL_QUERY)
+
+	assert resp.status_code == 500
+	assert resp.json()['detail'] == sql_constants.DB_CONNECT_FAILURE_MSG
