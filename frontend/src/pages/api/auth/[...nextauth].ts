9ce9efd96a85768402bbf75735e1390bb28ef782
Cynthia To <cvto1021@gmail.com>
2023-12-10 13:37:07 -0500

    Deleted the Apple provider

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,12 @@
 import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import AppleProvider from "next-auth/providers/apple";
+
 import FacebookProvider from "next-auth/providers/facebook";
 import NextAuth from "next-auth/next";
 
 export const options: NextAuthOptions = {
     providers: [
-        AppleProvider({
-            clientId: process.env.APPLE_ID as string,
-            clientSecret: process.env.APPLE_SECRET as string,
-        }),
         FacebookProvider({
             clientId: process.env.FACEBOOK_ID as string,
             clientSecret: process.env.FACEBOOK_SECRET as string,
